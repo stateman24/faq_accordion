761b2668f7869fc3802b160cf778dfb81155621e
Daniel Ajibewa <ajibewadannyboi@gmail.com>
2024-05-28 20:19:51 +0100

    Keyboard navigation feature added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ for( let i = 0; i < faq.length; i++ ) {
     faq[i].addEventListener('click', function (){
         this.classList.toggle('active')
         let image = this.querySelector('.question-box-box img')
-        if(image.src.includes("assets/images/icon-plus.svg")) {
+        if(this.classList.contains('active')) {
             image.src = "assets/images/icon-minus.svg"
         }
         else{
@@ -14,3 +14,40 @@ for( let i = 0; i < faq.length; i++ ) {
         }
     })
 }
+
+let currentIndex = 0
+document.addEventListener("keydown", function (event){
+    let image = faq[currentIndex].querySelector('.question-box-box img')
+    switch (event.key){
+        case 'ArrowDown':
+            currentIndex =(currentIndex + 1)% faq.length;
+            break;
+        case 'ArrowUp':
+            currentIndex = (currentIndex - 1)% faq.length;
+            if(currentIndex < 0){
+                currentIndex = 0;
+            }
+            break;
+        case 'ArrowLeft':
+            faq[currentIndex].classList.toggle('active');
+            if(image.src.includes("assets/images/icon-plus.svg")) {
+                image.src = "assets/images/icon-minus.svg";
+            }
+            else{
+                image.src = "assets/images/icon-plus.svg";
+            }
+            break;
+        case 'ArrowRight':
+            faq[currentIndex].classList.toggle('active');
+            if(image.src.includes("assets/images/icon-plus.svg")) {
+                image.src = "assets/images/icon-minus.svg";
+            }
+            else{
+                image.src = "assets/images/icon-plus.svg";
+            }
+            break;
+    }
+
+})
+
+
